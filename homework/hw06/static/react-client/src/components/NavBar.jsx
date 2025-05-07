b72088df97a4f17d61fd0734ae1a2b28144589e8
Violet <violetwoolard@gmail.com>
2025-05-07 11:54:06 -0400

    hw 6 yet again

--- homework/hw06/static/react-client/src/components/NavBar.jsx
+++ homework/hw06/static/react-client/src/components/NavBar.jsx
@@ -2,9 +2,6 @@ import React from "react";
 //export allows other files/things to look into this file 
 export default function NavBar({ username }) {
 
-    // function logout() {
-    //     alert("you only get one account :)");
-    // }
 
     // This component is implemented for you:
     return (
@@ -12,7 +9,7 @@ export default function NavBar({ username }) {
             <h1 className="font-Comfortaa font-bold text-2xl">Photo App</h1>
             <ul className="flex gap-4 text-sm items-center justify-center">
                 <li>
-                    <span className="text-black-700 py-2">{username}</span>
+                    <span className="text-black-700">{username}</span>
                 </li>
                 <li>
                     <button className="text-blue-700 py-2" aria-label="signout">Sign out</button>
